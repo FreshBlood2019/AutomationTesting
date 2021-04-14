ab5bb9bbeadd0c1c466e0a7f4c59522ca1911de6
natalliavalchkova <natalievolchkova@gmail.com>
2021-04-14 08:50:40 -0400

    First Commit

---
+++ AnnotationsTest.java
@@ -0,0 +1,50 @@
+import org.testng.annotations.*;
+
+public class AnnotationsTest {
+
+    @Test
+    public void testCase1(){
+        System.out.println("in test case 1");
+    }
+
+    // test case 2
+    @Test
+    public void testCase2() {
+        System.out.println("in test case 2");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("in afterMethod");
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("in before class");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        System.out.println("in afterClass");
+    }
+
+    @BeforeTest
+    public void beforeTest() {
+        System.out.println("in beforeTest");
+    }
+
+    @AfterTest
+    public void afterTest() {
+        System.out.println("in afterTest");
+    }
+
+    @BeforeSuite
+    public void beforeSuite() {
+        System.out.println("in beforeSuite");
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+        System.out.println("in afterSuite");
+    }
+}
